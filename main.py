599af4893b0861d8e4e53fa41b4fc5d8b6dbb992
Nick Loadholtes <nick@ironboundsoftware.com>
2025-05-25 15:10:25 -0400

    Serves one connection, echos back, dies.

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import socket
+
+
+class SimpleHTTPServer:
+    _socket = None
+
+    def __init__(self, hostname="", port=8080) -> None:
+        print("Initializing SimpleHTTPServer")
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.bind((hostname, port))
+
+    def serve(self):
+        if self._socket is None:
+            print("must initialize class first!")
+            return -1
+        self._socket.listen(1)
+        conn, addr = self._socket.accept()
+        with conn:
+            print("Ready for connection")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                # Print and echo
+                print(data)
+                conn.sendall(bytes(f"SERVER SAW: {data} \n".encode("utf8")))
+        print("Server stopping")
+
+
 def main():
     print("Hello from httpserver-simple!")
+    server = SimpleHTTPServer()
+    server.serve()
 
 
 if __name__ == "__main__":
